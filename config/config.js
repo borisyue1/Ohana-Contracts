da930e48d4645db38c2ec21c4e26d18e36915c42
Boris Yue <byue@byue-ltm1.internal.salesforce.com>
2018-07-23 16:10:04 -0700

    change default acct

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const url = "http://" + truffleConfig.host + ':' + truffleConfig.port;
 const Web3 = require('web3'),
 	  web3 = new Web3(new Web3.providers.HttpProvider(url));
 
-web3.eth.defaultAccount = "0x60b60b61e1400db6028abf8bf43e1e8a3c911fb0";
+web3.eth.defaultAccount = "0xef1a36234716baa97273078ca05aca96ebbcbc1d";
 
 module.exports = {
 	web3: {
