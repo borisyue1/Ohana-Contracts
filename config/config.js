e9175465ec1d4772520df95f8ac5be303912fef0
Boris Yue <byue@byue-ltm1.internal.salesforce.com>
2018-08-01 11:32:22 -0700

    redeploy with fast cache

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const url = "http://" + truffleConfig.host + ':' + truffleConfig.port;
 const Web3 = require('web3'),
 	  web3 = new Web3(new Web3.providers.HttpProvider(url));
 
-web3.eth.defaultAccount = "0x7f7e36010a62e022f234a1e5719a2dae9cd00c39";
+web3.eth.defaultAccount = "0x87479bb7a5fe790e062674ba718ac8b770872770";
 
 module.exports = {
 	web3: {
