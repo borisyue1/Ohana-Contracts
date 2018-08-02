63c30c5ca854673bc21e19d7ddc182d4b4ce5542
Boris Yue <byue@byue-ltm1.internal.salesforce.com>
2018-08-01 23:09:16 -0700

    change coinbase

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const url = "http://" + truffleConfig.host + ':' + truffleConfig.port;
 const Web3 = require('web3'),
 	  web3 = new Web3(new Web3.providers.HttpProvider(url));
 
-web3.eth.defaultAccount = "0x87479bb7a5fe790e062674ba718ac8b770872770";
+web3.eth.defaultAccount = "0x9c12270394a848c046bb85e62b85953019970394";
 
 module.exports = {
 	web3: {
